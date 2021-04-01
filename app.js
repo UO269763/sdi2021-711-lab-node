281b9fe3378b010a98f373c1e50d1ff208aba937
UO269763 <UO269763@uniovi.es>
2021-04-01 13:30:25 +0200

    SDI-2021-711-8.5-Colecciones relacionadas y routers

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 let express = require('express');
 let app = express();
 
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
 let crypto = require('crypto');
 
 let fileUpload = require('express-fileupload');
@@ -15,17 +22,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app,mongo);
 
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+
+//routerAudios
+let routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {"_id": mongo.ObjectID(idCancion) }, function (canciones) {
+            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerAudios
+app.use("/audios/",routerAudios);
+
 app.use(express.static('public'));
 
 //Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.wxhqv.mongodb.net:27017,tiendamusica-shard-00-01.wxhqv.mongodb.net:27017,tiendamusica-shard-00-02.wxhqv.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-9otbjb-shard-0&authSource=admin&retryWrites=true&w=majority');
-let expressSession = require('express-session');
-app.use(expressSession({
-    secret: 'abcdefg',
-    resave: true,
-    saveUninitialized: true
-}));
+
 app.set('clave','abcdefg');
 app.set('crypto',crypto);
 
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -47,5 +47,4 @@ module.exports = function(app, swig, gestorBD) {
         res.send("Usuario desconectado");
     })
 
-
 };
